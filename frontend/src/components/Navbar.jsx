b4b1940312b6177bfd76b6df9ce7fbcbd2a09d08
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-05-12 23:38:34 -0400

    Created new components

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+
+const Navbar = () => {
+
+    return (
+
+        <div className="navbar">
+            <div className="toggleButton">
+                <button></button>
+            </div>
+            <div className="links">
+                <Link to="/"> About </Link>
+                <Link to="/work"> Work </Link>
+                <Link to="/projects"> Projects </Link>
+                <Link to="/contact"> Contact </Link>
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
