195bb531223695d648a137402d628efeb01b16f1
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-05-13 15:48:21 -0400

    add user seeds, script to run reset

---
+++ backend/db/seeds/01_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password, admin)
+VALUES ('Jack Nicholson', 'jack@a.com', '123', FALSE),
+('Jack Torrance', 'shi@ning.com', 'allwork', FALSE),
+('Frank Costello', 'de@parted.com', 'boston', FALSE),
+('Randle McMurphy', 'one@flew.com', 'Ratched', FALSE);
+
+
+
