09afb36141c6f7fab261d165f5195cfc21b15e46
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-05-13 23:26:30 -0400

    addUser, getExperienceById queries

--- backend/db/queries/users.js
+++ backend/db/queries/users.js
@@ -14,4 +14,23 @@ const getUserById = id => {
 	})
 }
 
-module.exports = {getAllUsers, getUserById}
+const getExperienceById = id =>{
+  return db.query("SELECT * FROM experience; WHERE id = $1", [id]).then(data => {
+    return data.rows;
+  })
+}
+
+const addUser = (user) => {
+  const values = [user.name, user.email, user.password, NULL, FALSE];
+  return pool
+    .query(`INSERT INTO users (name, email, password, about, admin) VALUES ($1, $2, $3, $4, $5) RETURNING *;`, values)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log('add user error;', err.message);
+      return null;
+    });
+};
+
+module.exports = {getAllUsers, getUserById, getExperienceById, addUser}
