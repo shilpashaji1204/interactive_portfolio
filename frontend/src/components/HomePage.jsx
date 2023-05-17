8049a766ef9d600122b02cd618e9ff8f919b28e1
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-05-17 12:59:04 -0400

    Created new herosection

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -3,19 +3,14 @@ import About from "../pages/About";
 import Work from "../pages/Work";
 import Projects from "../pages/Projects";
 import Contact from "../pages/Contact";
-
+import HeroSection from "../pages/HeroSection";
 
 
 const HomePage = () => {
 
     return (
         <div className='home-page'>
-            <h1>Welcome to Our Interactive Portfolio!</h1>
-            <p>
-                This is where we showcase our projects, skills, and experience. Take a
-                look around and feel free to contact us if you have any questions or
-                would like to work together.
-            </p>
+            <HeroSection />
            <About />
            <Work />
            <Projects />
---
+++ frontend/src/pages/HeroSection.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/HeroSection.css";
+
+const HeroSection = () => {
+
+    return (
+        <wrapper>
+        <div className="container grid grid-two-column">
+            <div className="section-hero-data">
+                <h1 className="hero-heading">Welcome to Our Interactive Portfolio!</h1>
+                <p className="hero-para">
+                This is where we showcase our projects, skills, and experience. 
+                Take a look around and feel free to contact us if you have any questions or would like to work together. 
+                </p>
+                <Link to="/login">
+                <button className="hero-button">Get Started</button>
+                </Link>
+            </div>
+
+        </div>
+        </wrapper>
+    )
+}
+
+export default HeroSection;
