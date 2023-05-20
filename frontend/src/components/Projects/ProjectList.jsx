ba82d7ea3258b9e3b7348e1d1659bce806345cff
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-05-19 21:43:38 -0400

    update portfolio page to render projects for logged in user

--- frontend/src/components/Projects/ProjectList.jsx
+++ frontend/src/components/Projects/ProjectList.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import ProjectListItem from './ProjectListItem';
 
 export default function ProjectList(props) {
+
   console.log(props);
-  return (
-    <ProjectListItem
-      projectData={props.projectData}
-    />
-  )
+
+  const projectArray = props.projectData.map(project => {
+
+    return (
+      <ProjectListItem
+        key={project.id}
+        title={project.title}
+        description={project.description}
+        features={project.features}
+        tech_stack={project.tech_stack}
+        project_url={project.project_url}
+        image_url={project.image_url}
+      />
+    )
+  })
+
+  return <ul>{projectArray}</ul>
 }
--- frontend/src/components/Projects/ProjectListItem.jsx
+++ frontend/src/components/Projects/ProjectListItem.jsx
@@ -2,10 +2,21 @@ import React from "react";
 
 export default function ProjectListItem(props) {
 
-  console.log(props.projectData[0])
+  console.log(props);
+
+  const { title, description, features, tech_stack, project_url, image_url } = props;
+
   return (
     <div>
-      {props.projectData[0].id}
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+      <p>{features}</p>
+        <h3> Tech Stack: {tech_stack} </h3>
+        <h3> Links: </h3>
+      <ul> 
+        <li>{project_url} </li>
+        <li>{image_url} </li>
+      </ul>
     </div>
   )
 }
