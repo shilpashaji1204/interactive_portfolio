711b2249594f53a23adb7349eb94f5d4dbb23504
RorySchof <roryschofield@gmail.com>
2023-05-25 12:36:51 -0400

    Final demo

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,79 +1,321 @@
-import React from "react";
-import reactIcon from "../assets/images/react_icon.webp";
+import React, { useState } from "react";
 import "./About.css";
+import TechStack from "./TechStack"; // Import the TechStack component
 
-const About = () => {
+import pairPic from "../assets/images/pair_pic.jpg";
+
+const FlipCardPage = () => {
+  const [isFlipped1, setIsFlipped1] = useState(false);
+  const [isFlipped2, setIsFlipped2] = useState(false);
+  const [isFlipped3, setIsFlipped3] = useState(false);
+
+  const handleFlip1 = () => {
+    setIsFlipped1(!isFlipped1);
+  };
+
+  const handleFlip2 = () => {
+    setIsFlipped2(!isFlipped2);
+  };
+
+  const handleFlip3 = () => {
+    setIsFlipped3(!isFlipped3);
+  };
+
   return (
-    <div className="about">
-      {/* <h2>ABOUT US</h2> */}
-      <p>
-        <span className="intro">Introduction</span> <br />
-        <span className="overview">Overview.</span>
-        <br />
-        <br />
-        We are a full stack web development team that specializes in building cutting edge websites and web applications. If you can dream it, we can build it.
-        <br />
-        <br />
-        We use the most modern tech stack including HTML, CSS, JavaScript, and have expertise in frameworks like React, Node.js.
-        <br />
-        <br />
-        Let's work together to bring your ideas to life!
-        <br />
-        <br />
-
-        
-      </p>
-      {/* <div className="icon-container">
-        <img src={reactIcon} alt="React Icon" className="icon" />
-        <img src={reactIcon} alt="React Icon" className="icon" />
-        <img src={reactIcon} alt="React Icon" className="icon" />
-        <img src={reactIcon} alt="React Icon" className="icon" />
-      </div> */}
-
-      {/* Flip Cards */}
+    <div>
+      <h1>Flip Card Page</h1>
       <div className="flip-card-container">
-        <div className="flip-card">
-          <div className="flip-card-front">
-            <div className="image">
-              <img src="https://i.postimg.cc/nhG8H3X6/copywriting.jpg" alt="Copywriting" />
-              <h2>Website Development</h2>
+        <div className={`flip-card ${isFlipped1 ? "flipped" : ""}`} onClick={handleFlip1}>
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src={pairPic} alt="Card Front 1" />
+              <h2>Card Title 1</h2>
             </div>
-          </div>
-          <div className="flip-card-back">
-            <h2>Website Development</h2>
-            <p>Introduction to Copywriting workshop focuses on the theory and processes of professional copywriting as applied to persuasion, reasoning, and rhetoric. This workshop is best-suited to learning how to write and think about consumer-driven functions.</p>
+            <div className="flip-card-back">
+              <h2>Card Title 1</h2>
+              <p>Card Back Content 1</p>
+            </div>
           </div>
         </div>
-        <div className="flip-card">
-          <div className="flip-card-front">
-            <div className="image">
-              <img src="https://i.postimg.cc/ydrv1ZXq/contentmarketing.jpg" alt="Content Marketing" />
-              <h2>App Development</h2>
+        <div className={`flip-card ${isFlipped2 ? "flipped" : ""}`} onClick={handleFlip2}>
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src={pairPic} alt="Card Front 2" />
+              <h2>Card Title 2</h2>
             </div>
-          </div>
-          <div className="flip-card-back">
-            <h2>App Development</h2>
-            <p>Introduction to Content Marketing workshop focuses on building content frameworks that are designed for and directed at communication engagement. This interdisciplinary workshop is best-suited to learning visual and written communication strategies.</p>
+            <div className="flip-card-back">
+              <h2>Card Title 2</h2>
+              <p>Card Back Content 2</p>
+            </div>
           </div>
         </div>
-        <div className="flip-card">
-          <div className="flip-card-front">
-            <div className="image">
-              <img src="https://i.postimg.cc/ZqbG0630/webwriting.jpg" alt="Web Writing" />
-              <h2>Software Development</h2>
+        <div className={`flip-card ${isFlipped3 ? "flipped" : ""}`} onClick={handleFlip3}>
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src={pairPic} alt="Card Front 3" />
+              <h2>Card Title 3</h2>
             </div>
-          </div>
-          <div className="flip-card-back">
-            <h2>Software Development</h2>
-            <p>Introduction to Web-Writing workshop focuses on building creative and systemic digital content through online user experiences that benefit people and robots. This workshop is best-suited to creating content for digital platforms and devices — websites, mobile, game consoles, and virtual reality engines.</p>
+            <div className="flip-card-back">
+              <h2>Card Title 3</h2>
+              <p>Card Back Content 3</p>
+            </div>
           </div>
         </div>
       </div>
+      
+
+      {/* Add the TechStack component */}
+      {/* <TechStack /> */}
     </div>
   );
 };
 
-export default About;
+export default FlipCardPage;
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import "./About.css";
+// import Ball from "./Ball";
+// import BallCanvas from "./Ball";
+
+// import pairPic from "../assets/images/pair_pic.jpg";
+
+// const FlipCardPage = () => {
+//   const [isFlipped1, setIsFlipped1] = useState(false);
+//   const [isFlipped2, setIsFlipped2] = useState(false);
+//   const [isFlipped3, setIsFlipped3] = useState(false);
+
+//   const handleFlip1 = () => {
+//     setIsFlipped1(!isFlipped1);
+//   };
+
+//   const handleFlip2 = () => {
+//     setIsFlipped2(!isFlipped2);
+//   };
+
+//   const handleFlip3 = () => {
+//     setIsFlipped3(!isFlipped3);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Flip Card Page</h1>
+//       <div className="flip-card-container">
+//         <div className={`flip-card ${isFlipped1 ? "flipped" : ""}`} onClick={handleFlip1}>
+//           <div className="flip-card-inner">
+//             <div className="flip-card-front">
+//               <img src={pairPic} alt="Card Front 1" />
+//               <h2>Card Title 1</h2>
+//             </div>
+//             <div className="flip-card-back">
+//               <h2>Card Title 1</h2>
+//               <p>Card Back Content 1</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className={`flip-card ${isFlipped2 ? "flipped" : ""}`} onClick={handleFlip2}>
+//           <div className="flip-card-inner">
+//             <div className="flip-card-front">
+//               <img src={pairPic} alt="Card Front 2" />
+//               <h2>Card Title 2</h2>
+//             </div>
+//             <div className="flip-card-back">
+//               <h2>Card Title 2</h2>
+//               <p>Card Back Content 2</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className={`flip-card ${isFlipped3 ? "flipped" : ""}`} onClick={handleFlip3}>
+//           <div className="flip-card-inner">
+//             <div className="flip-card-front">
+//               <img src={pairPic} alt="Card Front 3" />
+//               <h2>Card Title 3</h2>
+//             </div>
+//             <div className="flip-card-back">
+//               <h2>Card Title 3</h2>
+//               <p>Card Back Content 3</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="ball-canvas-container">
+//         <BallCanvas icon={pairPic} />
+//         <BallCanvas icon={pairPic} />
+//         <BallCanvas icon={pairPic} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FlipCardPage;
+
+
+
+
+
+
+
+// working code below UPDATED. USE THIS
+
+
+// import React, { useState } from "react";
+// import "./About.css";
+
+// const FlipCardPage = () => {
+//   const [isFlipped1, setIsFlipped1] = useState(false);
+//   const [isFlipped2, setIsFlipped2] = useState(false);
+//   const [isFlipped3, setIsFlipped3] = useState(false);
+
+//   const handleFlip1 = () => {
+//     setIsFlipped1(!isFlipped1);
+//   };
+
+//   const handleFlip2 = () => {
+//     setIsFlipped2(!isFlipped2);
+//   };
+
+//   const handleFlip3 = () => {
+//     setIsFlipped3(!isFlipped3);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Flip Card Page</h1>
+//       <div className="flip-card-container">
+//         <div className={`flip-card ${isFlipped1 ? "flipped" : ""}`} onClick={handleFlip1}>
+//           <div className="flip-card-inner">
+//             <div className="flip-card-front">
+//               <img src="src/assets/images/pair_pic.jpg" alt="Card Front 1" />
+//               <h2>Card Title 1</h2>
+//             </div>
+//             <div className="flip-card-back">
+//               <h2>Card Title 1</h2>
+//               <p>Card Back Content 1</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className={`flip-card ${isFlipped2 ? "flipped" : ""}`} onClick={handleFlip2}>
+//           <div className="flip-card-inner">
+//             <div className="flip-card-front">
+//               <img src="src/assets/images/pair_pic.jpg" alt="Card Front 2" />
+//               <h2>Card Title 2</h2>
+//             </div>
+//             <div className="flip-card-back">
+//               <h2>Card Title 2</h2>
+//               <p>Card Back Content 2</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className={`flip-card ${isFlipped3 ? "flipped" : ""}`} onClick={handleFlip3}>
+//           <div className="flip-card-inner">
+//             <div className="flip-card-front">
+//               <img src="src/assets/images/pair_pic.jpg" alt="Card Front 3" />
+//               <h2>Card Title 3</h2>
+//             </div>
+//             <div className="flip-card-back">
+//               <h2>Card Title 3</h2>
+//               <p>Card Back Content 3</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FlipCardPage;
+
+
+
+
+
+
+
+
+// Working code
+
+
+// import React from "react";
+// import reactIcon from "../assets/images/react_icon.webp";
+// import "./About.css";
+
+// const About = () => {
+//   return (
+//     <div className="about">
+//       {/* <h2>ABOUT US</h2> */}
+//       <p>
+//         <span className="intro">Introduction</span> <br />
+//         <span className="overview">Overview.</span>
+//         <br />
+//         <br />
+//         We are a full stack web development team that specializes in building cutting edge websites and web applications. If you can dream it, we can build it.
+//         <br />
+//         <br />
+//         We use the most modern tech stack including HTML, CSS, JavaScript, and have expertise in frameworks like React, Node.js.
+//         <br />
+//         <br />
+//         Let's work together to bring your ideas to life!
+//         <br />
+//         <br />
+
+        
+//       </p>
+//       {/* <div className="icon-container">
+//         <img src={reactIcon} alt="React Icon" className="icon" />
+//         <img src={reactIcon} alt="React Icon" className="icon" />
+//         <img src={reactIcon} alt="React Icon" className="icon" />
+//         <img src={reactIcon} alt="React Icon" className="icon" />
+//       </div> */}
+
+//       {/* Flip Cards */}
+//       <div className="flip-card-container">
+//         <div className="flip-card">
+//           <div className="flip-card-front">
+//             <div className="image">
+//               <img src="https://i.postimg.cc/nhG8H3X6/copywriting.jpg" alt="Copywriting" />
+//               <h2>Website Development</h2>
+//             </div>
+//           </div>
+//           <div className="flip-card-back">
+//             <h2>Website Development</h2>
+//             <p>Introduction to Copywriting workshop focuses on the theory and processes of professional copywriting as applied to persuasion, reasoning, and rhetoric. This workshop is best-suited to learning how to write and think about consumer-driven functions.</p>
+//           </div>
+//         </div>
+//         <div className="flip-card">
+//           <div className="flip-card-front">
+//             <div className="image">
+//               <img src="https://i.postimg.cc/ydrv1ZXq/contentmarketing.jpg" alt="Content Marketing" />
+//               <h2>App Development</h2>
+//             </div>
+//           </div>
+//           <div className="flip-card-back">
+//             <h2>App Development</h2>
+//             <p>Introduction to Content Marketing workshop focuses on building content frameworks that are designed for and directed at communication engagement. This interdisciplinary workshop is best-suited to learning visual and written communication strategies.</p>
+//           </div>
+//         </div>
+//         <div className="flip-card">
+//           <div className="flip-card-front">
+//             <div className="image">
+//               <img src="https://i.postimg.cc/ZqbG0630/webwriting.jpg" alt="Web Writing" />
+//               <h2>Software Development</h2>
+//             </div>
+//           </div>
+//           <div className="flip-card-back">
+//             <h2>Software Development</h2>
+//             <p>Introduction to Web-Writing workshop focuses on building creative and systemic digital content through online user experiences that benefit people and robots. This workshop is best-suited to creating content for digital platforms and devices — websites, mobile, game consoles, and virtual reality engines.</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default About;
 
 
 
--- frontend/src/pages/TechStack.jsx
+++ frontend/src/pages/TechStack.jsx
@@ -1,38 +1,252 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+
 import "./TechStack.css";
+import reactIcon from "../pages/react_icon.webp";
 
 const TechStack = () => {
-  return (
-    <div className="tech-stack">
-      <h2>Tech Stack</h2>
-      <p>
-        We use the following technologies. 
-      </p>
-      <div className="circle-container">
-        <div className="circle-wrapper">
-          <div className="circle">HTML</div>
-        </div>
-        <div className="circle-wrapper">
-          <div className="circle">CSS</div>
-        </div>
-        <div className="circle-wrapper">
-          <div className="circle">JS</div>
-        </div>
-        <div className="circle-wrapper">
-          <div className="circle">REACT</div>
-        </div>
-        <div className="circle-wrapper">
-          <div className="circle">3JS</div>
-        </div>
-        <div className="circle-wrapper">
-          <div className="circle">EXPRESS</div>
-        </div>
-        <div className="circle-wrapper">
-          <div className="circle">RUBY</div>
-        </div>
-      </div>
-    </div>
-  );
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    // Create a scene
+    const scene = new THREE.Scene();
+
+    // Create a camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 10;
+
+    // Create a renderer
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Add the renderer to the HTML canvas
+    canvas.appendChild(renderer.domElement);
+
+    // Create cubes for each technology
+    const technologies = [
+      "HTML",
+      "CSS",
+      "JS",
+      "REACT",
+      "3JS",
+      "EXPRESS",
+      "RUBY",
+    ];
+
+    const cubes = [];
+    const cubeSize = 2;
+
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load(reactIcon);
+
+    // Calculate the total height occupied by the cubes
+    const totalHeight = cubeSize * technologies.length;
+
+    technologies.forEach((technology, index) => {
+      const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+      const cubeMaterial = new THREE.MeshBasicMaterial({
+        map: texture,
+        transparent: true,
+        opacity: 1,
+      });
+      const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+      cube.position.x = (index - technologies.length / 2) * 4;
+
+      // Set the y position to keep the cubes at the bottom of the screen
+      cube.position.y = -totalHeight / 2;
+
+      scene.add(cube);
+      cubes.push(cube);
+    });
+
+    // Animation loop
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      // Rotate cubes
+      cubes.forEach((cube) => {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+      });
+
+      // Render the scene with the camera
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    // Update renderer size on window resize
+    const handleWindowResize = () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+      canvas.removeChild(renderer.domElement);
+    };
+  }, []);
+
+  return <div ref={canvasRef} className="canvas-container" />;
 };
 
 export default TechStack;
+
+
+
+
+
+// Woriking w black background
+
+// import React, { useEffect, useRef } from "react";
+// import * as THREE from "three";
+
+// import "./TechStack.css";
+// import reactIcon from "../pages/react_icon.webp";
+
+// const TechStack = () => {
+//   const canvasRef = useRef(null);
+
+//   useEffect(() => {
+//     const canvas = canvasRef.current;
+
+//     // Create a scene
+//     const scene = new THREE.Scene();
+
+//     // Create a camera
+//     const camera = new THREE.PerspectiveCamera(
+//       75,
+//       window.innerWidth / window.innerHeight,
+//       0.1,
+//       1000
+//     );
+//     camera.position.z = 10;
+
+//     // Create a renderer
+//     const renderer = new THREE.WebGLRenderer({ antialias: true });
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+
+//     // Add the renderer to the HTML canvas
+//     canvas.appendChild(renderer.domElement);
+
+//     // Create cubes for each technology
+//     const technologies = [
+//       "HTML",
+//       "CSS",
+//       "JS",
+//       "REACT",
+//       "3JS",
+//       "EXPRESS",
+//       "RUBY",
+//     ];
+
+//     const cubes = [];
+//     const cubeSize = 2;
+
+//     const textureLoader = new THREE.TextureLoader();
+//     const texture = textureLoader.load(reactIcon);
+
+//     technologies.forEach((technology, index) => {
+//       const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+//       const cubeMaterial = new THREE.MeshBasicMaterial({
+//         map: texture,
+//       });
+//       const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+//       cube.position.x = (index - technologies.length / 2) * 4;
+//       scene.add(cube);
+//       cubes.push(cube);
+//     });
+
+//     // Animation loop
+//     const animate = () => {
+//       requestAnimationFrame(animate);
+
+//       // Rotate cubes
+//       cubes.forEach((cube) => {
+//         cube.rotation.x += 0.01;
+//         cube.rotation.y += 0.01;
+//       });
+
+//       // Render the scene with the camera
+//       renderer.render(scene, camera);
+//     };
+
+//     animate();
+
+//     // Update renderer size on window resize
+//     const handleWindowResize = () => {
+//       const width = window.innerWidth;
+//       const height = window.innerHeight;
+//       renderer.setSize(width, height);
+//       camera.aspect = width / height;
+//       camera.updateProjectionMatrix();
+//     };
+
+//     window.addEventListener("resize", handleWindowResize);
+
+//     // Cleanup
+//     return () => {
+//       window.removeEventListener("resize", handleWindowResize);
+//       canvas.removeChild(renderer.domElement);
+//     };
+//   }, []);
+
+//   return <div ref={canvasRef} className="canvas-container" />;
+// };
+
+// export default TechStack;
+
+
+// working. rough
+
+// import React from "react";
+// import "./TechStack.css";
+
+// const TechStack = () => {
+//   return (
+//     <div className="tech-stack">
+//       <h2>Tech Stack</h2>
+//       <p>
+//         We use the following technologies. 
+//       </p>
+//       <div className="circle-container">
+//         <div className="circle-wrapper">
+//           <div className="circle">HTML</div>
+//         </div>
+//         <div className="circle-wrapper">
+//           <div className="circle">CSS</div>
+//         </div>
+//         <div className="circle-wrapper">
+//           <div className="circle">JS</div>
+//         </div>
+//         <div className="circle-wrapper">
+//           <div className="circle">REACT</div>
+//         </div>
+//         <div className="circle-wrapper">
+//           <div className="circle">3JS</div>
+//         </div>
+//         <div className="circle-wrapper">
+//           <div className="circle">EXPRESS</div>
+//         </div>
+//         <div className="circle-wrapper">
+//           <div className="circle">RUBY</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TechStack;
