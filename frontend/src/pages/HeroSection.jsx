695016d4316c556990f8dc3cdc9050643642e9d0
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-05-25 12:09:52 -0400

    Modified css

--- frontend/src/pages/HeroSection.jsx
+++ frontend/src/pages/HeroSection.jsx
@@ -6,61 +6,26 @@ import "../styles/HeroSection.css";
 import Cube from "../pages/Cube";
 
 const HeroSection = () => {
-
-    return (
-        <wrapper>
-        <div className="container grid grid-two-column">
-            <div className="section-hero-data">
-                <h1 className="hero-heading">Welcome to Our Interactive Portfolio!</h1>
-                <p className="hero-para">
-                This is where we showcase our projects, skills, and experience. 
-                Take a look around and feel free to contact us if you have any questions or would like to work together. 
-                </p>
-                <Link to="/login">
-                <button className="hero-button">Get Started</button>
-                </Link>
-            </div>
-
+  return (
+    <wrapper>
+      <div className="container grid grid-two-column">
+        <div className="section-hero-data">
+          <h1 className="hero-heading">Welcome to Our Interactive Portfolio!</h1>
+          <p className="hero-para">
+            This is where we showcase our projects, skills, and experience.
+            Take a look around and create your own Portfolio. Contact us if you have any questions or would like to work together.
+          </p>
+          {/* <Link to="/login">
+            <button className="hero-button">Get Started</button>
+          </Link> */}
         </div>
         <div className="section-hero-animation">
           <Lottie animationData={animationData} />
           <Cube />
         </div>
-        </wrapper>
-      
+      </div>
+    </wrapper>
   );
 };
 
 export default HeroSection;
-
-
-
-// // ORGINAIL CODE (SH) BELOW
-
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import "../styles/HeroSection.css";
-
-// const HeroSection = () => {
-
-//     return (
-//         <wrapper>
-//         <div className="container grid grid-two-column">
-//             <div className="section-hero-data">
-//                 <h1 className="hero-heading">Welcome to Our Interactive Portfolio!</h1>
-//                 <p className="hero-para">
-//                 This is where we showcase our projects, skills, and experience. 
-//                 Take a look around and create your own Portfolio. Contact us if you have any questions or would like to work together. 
-//                 </p>
-//                 <Link to="/login">
-//                 <button className="hero-button">Get Started</button>
-//                 </Link>
-//             </div>
-
-//         </div>
-//         </wrapper>
-//     )
-// }
-
-// export default HeroSection;
