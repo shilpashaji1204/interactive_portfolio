c363feb66da1dc215273484e91b67dcd9834d487
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-05-25 21:38:06 -0400

    additional README info, fix projects styling

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -14,7 +14,8 @@ const Projects = () => {
   const {allProjects} = useAppData();
 
   const projectsArray = allProjects.map(project => (
-    <ProjectsListItem  
+    <div>
+     <ProjectsListItem  
       key={project.id}
       title={project.title}
       description={project.description}
@@ -23,6 +24,8 @@ const Projects = () => {
       project_url={project.project_url}
       image_url={project.image_url}
     />
+    <div className="spacer"></div>
+    </div>
   ));
 
   return (
@@ -32,92 +35,6 @@ const Projects = () => {
       {projectsArray}
     </div>
   
-    
-  //   <div className="project-container">
-
-
-  //     {/* <h2>PROJECTS!!!!!</h2> */}
-  //     <p className="project-text">
-  //       Take a look at some of our past client work. These reflect our ability to solve complex problems, work with different technologies, and manage
-  //       projects effectively. We invite you click the link to see the work in action or explore our code. 
-  //     </p>
-
-  //     <div className="projects">
-  //       <div className="project-card">
-  //         <div className="flip-card">
-  //           <div className="flip-card-front">
-  //             <div className="image">
-  //               <img src={schedulerImage} alt="Pair of people working" className="project-image" />
-  //               <h2>SCHEDULER</h2>
-  //             </div>
-  //           </div>
-  //           <div className="flip-card-back">
-  //             <h2>SCHEDULER</h2>
-  //             <p>
-  //               Web-based platform that allows users to search, book, and manage car rentals from various providers,
-  //               providing a convenient and efficient solution for transportation needs.
-  //             </p>
-  //             <span>#react</span>
-
-  //             <div className="buttons">
-  //   <button className="button">View Project</button>
-  //   <Link to={'https://github.com/7-O-2-H/scheduler'} >
-  //     <button className="button">Explore Code</button>
-  //   </Link>  </div>
-  //           </div>
-  //         </div>
-  //       </div>
-
-  //       <div className="project-card">
-  //         <div className="flip-card">
-  //           <div className="flip-card-front">
-  //             <div className="image">
-  //               <img src={jungleImage} alt="Pair of people working" className="project-image" />
-  //               <h2>JUNGLE</h2>
-  //             </div>
-  //           </div>
-  //           <div className="flip-card-back">
-  //             <h2>JUNGLE</h2>
-  //             <p>
-  //               Web application that enables users to search for job openings, view estimated salary ranges for positions,
-  //               and locate available jobs based on their current location.
-  //             </p>
-
-  //             <div className="buttons">
-  //   <button className="button">View Project</button>
-  //   <Link to={"https://github.com/7-O-2-H/jungle"} >
-  //     <button className="button">Explore Code</button>
-  //   </Link>
-  // </div>
-  //           </div>
-  //         </div>
-  //       </div>
-
-  //       <div className="project-card">
-  //         <div className="flip-card">
-  //           <div className="flip-card-front">
-  //             <div className="image">
-  //               <img src={tinyImage} alt="Pair of people working" className="project-image" />
-  //               <h2>TINY APP</h2>
-  //             </div>
-  //           </div>
-  //           <div className="flip-card-back">
-  //             <h2>TINY APP</h2>
-  //             <p>
-  //               Web application that enables users to search for job openings, view estimated salary ranges for positions,
-  //               and locate available jobs based on their current location.
-  //             </p>
-  //             <div className="buttons">
-  //   <button className="button">View Project</button>
-  //   <Link to={"https://github.com/RorySchof/tinyapp"} >
-  //     <button className="button">Explore Code</button>
-  //   </Link>
-  // </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
   )
 };
 
@@ -130,57 +47,3 @@ export default Projects;
 
 
 
-// import React from "react";
-// import "./Projects.css";
-// import pairPic from "../assets/images/pair_pic.jpg";
-
-// const Projects = () => {
-//   return (
-//     <div className="about project-container">
-//       <h2>PROJECTS!!!!!</h2>
-//       <p className="about-text project-text">
-//         Take a look at some of our past clinet work. Each project is
-//         briefly described with links to code repositories and live demos. These reflect our ability to solve complex
-//         problems, work with different technologies, and manage projects effectively.
-//       </p>
-
-//       <div className="projects">
-//         <div className="project-card">
-//           <img src={pairPic} alt="Pair of people working" className="project-image" />
-//           <div className="project-description">
-//             SCHEDULER
-//             <br />
-//             Web-based platform that allows users to search, book, and manage car rentals from various providers,
-//             providing a convenient and efficient solution for transportation needs.
-//             <br />
-//             #react
-//             <br />
-           
-//           </div>
-//         </div>
-
-//         <div className="project-card">
-//           <img src={pairPic} alt="Pair of people working" className="project-image" />
-//           <div className="project-description">
-//             JUNGLE
-//             <br />
-//             Web application that enables users to search for job openings, view estimated salary ranges for positions,
-//             and locate available jobs based on their current location.
-//           </div>
-//         </div>
-
-//         <div className="project-card">
-//           <img src={pairPic} alt="Pair of people working" className="project-image" />
-//           <div className="project-description">
-//             TINY APP
-//             <br />
-//             Web application that enables users to search for job openings, view estimated salary ranges for positions,
-//             and locate available jobs based on their current location.
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Projects;
--- frontend/src/pages/ProjectsListItem.jsx
+++ frontend/src/pages/ProjectsListItem.jsx
@@ -7,34 +7,36 @@ export default function ProjectsListItem(props) {
   const { title, description, features, tech_stack, project_url, image_url } = props;
 
   return (
+  
+  <div className="projects-page">
+      <div className="project-card-container">
 
-    <div className="flip-card-container">
-
-      <div className="projects">
-        <div className="project-card">
-          <div className="flip-card">
-            <div className="flip-card-front">
-              <div className="image">
-                <img src={image_url} className="project-image" />
-                <h2>{title}</h2>
+        <div className="projects">
+          <div className="project-card">
+            <div className="flip-card">
+              <div className="flip-card-front">
+                <div className="image">
+                  <img src={image_url} className="project-image" />
+                </div>
               </div>
-            </div>
-            <div className="flip-card-back">
-              <h2>{title}</h2>
-              <p>
-                {description}
-              </p>
+              <div className="flip-card-back">
+                <h2>{title}</h2>
+                <p>
+                  {description}
+                </p>
 
-              <div className="buttons">
-                <button className="button">View Project</button>
-                <Link to={project_url} >
-                  <button className="button">Explore Code</button>
-                </Link>  
+                <div className="buttons">
+                  <button className="button">View Project</button>
+                  <Link to={project_url} >
+                    <button className="button">Explore Code</button>
+                  </Link>  
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <h2 className="title">{title}</h2>
+    </div>  
   )
 }
